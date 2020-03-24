f8033982e4a5ddcc99f15160912efb9dc3a8fb63
Ananya Yadav <yadav.ana@husky.neu.edu>
2020-03-24 15:26:33 -0400

    Assignment9 changes

--- initialsetup.sh
+++ initialsetup.sh
@@ -2,3 +2,4 @@ sudo apt-get update
 sudo apt-get install curl
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
+curl -L https://npmjs.org/install.sh | sudo sh
